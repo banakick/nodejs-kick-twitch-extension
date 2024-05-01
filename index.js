eecda233c3c363c0e05e0fc4753484c947ae58ac
banakick <167943753+banakick@users.noreply.github.com>
2024-05-01 20:30:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const mysql = require('mysql');
-const cors = require('cors');
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
