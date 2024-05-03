69a6cd03f58378f6edb7fe353b4a72f3e8a005c1
banakick <167943753+banakick@users.noreply.github.com>
2024-05-03 18:43:09 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
+import sqlite3 from 'sqlite3';
 const db = new sqlite3.Database('users.db');
 
 db.serialize(() => {
   db.run('CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, points INTEGER)');
 });
 
-module.exports = db;
+export default db;
